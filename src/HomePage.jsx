71070b51a21bd6b952f46fd9c33d02024edcbc4b
rodman <rodman.com>
2025-10-02 15:04:28 -0500

    ONCE NUEVE e2b98fb

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,28 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { supabase } from './supabaseClient';
-import { useAuth } from './AuthContext';
-
 import RegisterForm from './RegisterForm'; 
 import LoginForm from './LoginForm';
 
 export default function HomePage() {
-  const navigate = useNavigate();
-  const { setUser } = useAuth();
-
-  useEffect(() => {
-    const handleAuthRedirect = async () => {
-      if (window.location.hash.includes('access_token')) {
-        // Opcional: limpiar cualquier sesión previa
-        await supabase.auth.getSession(); // solo para que Supabase inicialice su estado
-        setUser(null); // Por si acaso había una sesión
-        navigate('/login');
-      }
-    };
-
-    handleAuthRedirect();
-  }, [navigate, setUser]);
-
   return (
     <div style={{ maxWidth: '400px', margin: '0 auto', padding: '2rem' }}>
       <h1 style={{ textAlign: 'center' }}>Bienvenido</h1>
@@ -39,7 +18,6 @@ export default function HomePage() {
     </div>
   );
 }
-
 
 
 
